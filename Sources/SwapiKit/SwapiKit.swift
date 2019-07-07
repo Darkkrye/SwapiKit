b024f6555b93f4af758d1b514ea3152261698f5e
Darkkrye <pierre.boudon.27@gmail.com>
2019-07-07 15:12:18 +0200

    Initial Commit

--- Sources/SwapiKit/SwapiKit.swift
+++ Sources/SwapiKit/SwapiKit.swift
@@ -1,3 +1,21 @@
-struct SwapiKit {
+//
+//   _____             _    _
+//  |  __ \           | |  | |
+//  | |  | | __ _ _ __| | _| | ___ __ _   _  ___
+//  | |  | |/ _` | '__| |/ / |/ / '__| | | |/ _ \
+//  | |__| | (_| | |  |   <|   <| |  | |_| |  __/
+//  |_____/ \__,_|_|  |_|\_\_|\_\_|   \__, |\___|
+//                                     __/ |
+//                                    |___/
+//
+//  SwapiKit.swift
+//  
+//
+//  Created by Pierre on 07/07/2019.
+//
+
+import Foundation
+
+class SwapiKit: NSObject {
     var text = "Hello, World!"
 }
